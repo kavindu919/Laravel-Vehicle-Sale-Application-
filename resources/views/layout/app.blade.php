ec82486b8678a3946dc1d4605512a679f90895a5
kavindu919 <jayakodyk2@gmail.com>
2024-11-17 09:28:27 +0530

    Start Making Basic Controllers

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.clean')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title') | {{ config('app.name') }}</title>
-</head>
-
-<body>
-    <header>My Header</header>
+@section('childContent')
+    @include('layout.partials.header')
     @yield('content')
-    <footer>My Footer</footer>
-</body>
-
-</html>
+@endsection
+<footer></footer>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,'index'])->name('home');
 
+Route::get('/signup',[SignupController::class,'create']);
 
