56984dbf612a93b08dc3033a33a8ee600a68b71e
kavindu919 <jayakodyk2@gmail.com>
2024-11-20 23:19:37 +0530

    Initail car controller and view

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,10 +2,5 @@
 <x-base-layout :$title>
     <x-layout.header />
     {{ $slot }}
-    <footer>
-        <a href="#">Link 1</a>
-        <a href="#">Link 2</a>
-        {{ $footerLinks }}
 
-    </footer>
 </x-base-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/signup',[SignupController::class,'create']);
-Route::get('/login',[LoginController::class,'create']);
+Route::get('/car/search', [CarController::class, 'search'])->name('class.search');
+Route::resource('car', CarController::class);
+
+Route::get('/signup', [SignupController::class, 'create'])->name('signup');
+Route::get('/login', [LoginController::class, 'create'])->name('login');
