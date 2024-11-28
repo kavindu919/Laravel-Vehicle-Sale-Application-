4d86c5aaa4adc556bbb2ad41e3724c45c89d5621
kavindu919 <jayakodyk2@gmail.com>
2024-11-28 22:10:52 +0530

    Complete all the models fillable array

--- app/Models/CarFeatures.php
+++ app/Models/CarFeatures.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CarFeatures extends Model
 {
-    //
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = [
+        'car_id',
+        'abs',
+        'air_conditioning',
+        'power_windows',
+        'power_door_locks',
+        'cruise_control',
+        'bluetooth_connectivity',
+        'remote_strat',
+        'gps_navigation',
+        'heated_seats',
+        'climate_control',
+        'rear_parking_sensors',
+        'leather_seates',
+    ];
 }
--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
 
-class Model extends Model
+class Model extends EloquentModel
 {
-    //
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = ['name', 'maker_id'];
 }
